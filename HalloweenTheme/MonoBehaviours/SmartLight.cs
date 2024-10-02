ecee49e5aa82d4ce05f25b5b7398a47fd913d2d7
Zehs <andrey5764zehsteam@gmail.com>
2024-10-02 11:59:57 -0500

    Release v1.1.2
    * Added some decorations to 8 Titan.
    * Some performance improvements.

---
+++ HalloweenTheme/MonoBehaviours/SmartLight.cs
@@ -0,0 +1,22 @@
+﻿using GameNetcodeStuff;
+using UnityEngine;
+
+namespace com.github.zehsteam.HalloweenTheme.MonoBehaviours;
+
+public class SmartLight : MonoBehaviour
+{
+    public Light Light;
+    public bool DisableAfterMaxDistance = true;
+    public float MaxDistance = 60f;
+
+    private void Update()
+    {
+        if (Light == null) return;
+        if (!DisableAfterMaxDistance) return;
+
+        PlayerControllerB playerScript = PlayerUtils.GetLocalPlayerScript();
+        if (playerScript == null) return;
+
+        Light.enabled = Vector3.Distance(transform.position, playerScript.transform.position) <= MaxDistance;
+    }
+}
---
+++ HalloweenTheme/PlayerUtils.cs
@@ -0,0 +1,17 @@
+﻿using GameNetcodeStuff;
+
+namespace com.github.zehsteam.HalloweenTheme;
+
+internal static class PlayerUtils
+{
+    public static PlayerControllerB GetLocalPlayerScript()
+    {
+        if (GameNetworkManager.Instance == null) return null;
+        return GameNetworkManager.Instance.localPlayerController;
+    }
+
+    public static bool IsLocalPlayer(PlayerControllerB playerScript)
+    {
+        return playerScript == GetLocalPlayerScript();
+    }
+}
